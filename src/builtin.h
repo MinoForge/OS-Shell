f905f8c3be3f09a926481401f482e565cfe5d9b7
cwdinehart1 <37390080+cwdinehart1@users.noreply.github.com>
2019-03-22 17:41:37 -0400

    Update builtin.h

--- src/builtin.h
+++ src/builtin.h
@@ -1,3 +1,10 @@
+/**
+ * Header file for builtin shell functions.
+ *
+ * @author Peter Gardner
+ * @author Caleb Dinehart
+ * @version March 22, 2019
+ */
 #ifndef BUILTIN_H
     #define BUILTIN_H
     void   do_pipe(char** , char** , int*);
