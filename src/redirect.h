6e322c5241887531030f29f639681699ecfbc9e4
cwdinehart1 <37390080+cwdinehart1@users.noreply.github.com>
2019-03-22 17:45:12 -0400

    Update redirect.h

--- src/redirect.h
+++ src/redirect.h
@@ -1,3 +1,10 @@
+/**
+ * Header file for redirection.
+ *
+ * @author Peter Gardner
+ * @author Caleb Dinehart
+ * @version March 22, 2019
+ */
 #ifndef REDIRECT_H
    #define REDIRECT_H
     void   append_redirection(char*);
